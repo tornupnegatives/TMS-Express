b54c403628313119bd49815308d702c79899bce4
Joseph Bellahcen <joeclb@icloud.com>
2023-04-27 13:20:00 -0400

    Updated CMake project structure

--- test/TmsTest.cmake
+++ test/TmsTest.cmake
@@ -1,8 +1,9 @@
 cmake_minimum_required(VERSION 3.14)
 set(CMAKE_CXX_STANDARD 14)
 
-include(cmake/FindJSON.cmake)
-
+#  ====================================
+#          Google Test Framework
+#  ====================================
 include(FetchContent)
 FetchContent_Declare(googletest
         GIT_REPOSITORY "https://github.com/google/googletest.git"
@@ -12,6 +13,14 @@ FetchContent_Declare(googletest
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(googletest)
 enable_testing()
+
+#  ====================================
+#             PROGRAM FILES
+#  ====================================
+include_directories(
+        inc
+        lib
+)
 
 add_executable(TMS-Test
         src/LPC_Analysis/Autocorrelator.cpp
@@ -26,10 +35,8 @@ add_executable(TMS-Test
 
 target_link_libraries(
         TMS-Test
-        nlohmann_json::nlohmann_json
         gtest_main
 )
 
 include(GoogleTest)
 gtest_discover_tests(TMS-Test)
-
