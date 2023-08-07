11bdd582b9151eca8e112ca1e75c1704074488bd
Joseph Bellahcen <joeclb@icloud.com>
2023-08-06 20:15:38 -0700

    Re-factored tests

--- test/AutocorrelatorTests.cpp
+++ test/AutocorrelatorTests.cpp
@@ -1,20 +1,26 @@
-//
-// Created by Joseph Bellahcen on 6/1/22.
-//
+// Copyright 2023 Joseph Bellahcen <joeclb@icloud.com>
 
-#include "LPC_Analysis/Autocorrelation.hpp"
+#include <gtest/gtest.h>
+
 #include <algorithm>
 #include <cmath>
-#include <gtest/gtest.h>
 #include <vector>
+
+#include "LPC_Analysis/Autocorrelation.hpp"
 
 namespace tms_express {
 
-// Compute the autocorrelation of a decaying cosine signal with amplitude 20 and period 50
+/// @brief Produces test subject, which is the autocorrelation of a decaying
+///         cosine signal with amplitude 20 and period 50
+/// @return Test subject
 std::vector<float> acfTestSubject() {
     auto signal = std::vector<float>();
     for (int i = 0; i < 200; i ++) {
-        float sample = 20.0f * cosf(2.0f * float(M_PI) * float(i) / 50.0f) * expf(-0.02f * float(i));
+        float sample = 20.0f *
+            cosf(2.0f * static_cast<float>(M_PI) *
+            static_cast<float>(i) / 50.0f) *
+            expf(-0.02f * static_cast<float>(i));
+
         signal.push_back(sample);
     }
 
@@ -22,27 +28,24 @@ std::vector<float> acfTestSubject() {
     return acf;
 }
 
-// The autocorrelation should be maximized at time zero, or pitch detection will fail
 TEST(AutocorrelatorTests, AutocorrelationIsMaxAtIndexZero) {
     auto acf = acfTestSubject();
 
-    auto maxElement = std::max_element(acf.begin(), acf.end());
-    auto maxIdx = std::distance(acf.begin(), maxElement);
-    EXPECT_EQ(maxIdx, 0);
+    auto max_element = std::max_element(acf.begin(), acf.end());
+    auto max_idx = std::distance(acf.begin(), max_element);
+    EXPECT_EQ(max_idx, 0);
 }
 
-// For a periodic signal, the autocorrelation should have its second-largest local max at the period. This property
-// is the basis of many pitch estimation algorithms
 TEST(AutocorrelatorTests, AutocorrelationHasLocalMaxAtOriginalSignalPeriod) {
     auto acf = acfTestSubject();
 
-    auto maxElement = std::max_element(acf.begin(), acf.end());
-    auto maxIdx = std::distance(acf.begin(), maxElement);
+    auto max_element = std::max_element(acf.begin(), acf.end());
+    auto max_idx = std::distance(acf.begin(), max_element);
 
-    auto minElement = std::min_element(acf.begin(), acf.end());
-    auto nextMaxElement = std::max_element(minElement, acf.end());
-    auto periodIdx = std::distance(acf.begin(), nextMaxElement);
-    EXPECT_NEAR(periodIdx, 50, 2);
+    auto min_element = std::min_element(acf.begin(), acf.end());
+    auto next_max_element = std::max_element(min_element, acf.end());
+    auto period_idx = std::distance(acf.begin(), next_max_element);
+    EXPECT_NEAR(period_idx, 50, 2);
 }
 
 };  // namespace tms_express
--- test/FrameTests.cpp
+++ test/FrameTests.cpp
@@ -1,17 +1,20 @@
-#include "Frame_Encoding/Frame.hpp"
+// Copyright 2023 Joseph Bellahcen <joeclb@icloud.com>
+
 #include <gtest/gtest.h>
+
 #include <vector>
+
+#include "Frame_Encoding/Frame.hpp"
 
 namespace tms_express {
 
 Frame frameTestSubject() {
-    return Frame(38, true, 56.850773, {-0.753234, 0.939525, -0.342255, -0.172317,
-                                                                        0.108887, 0.679660, 0.056874, 0.433271,
-                                                                        -0.220355, 0.17028});
+    return Frame(38, true, 56.850773,
+        {-0.753234, 0.939525, -0.342255, -0.172317, 0.108887, 0.679660,
+            0.056874, 0.433271, -0.220355, 0.17028});
 }
 
-// A silent frame should contain only an energy parameter
-TEST(FrameTests, SilentFrame) {
+TEST(FrameTests, SilentFrameContainsOnlyEnergyParameter) {
     auto frame = frameTestSubject();
     frame.setGain(0.0f);
 
@@ -19,110 +22,102 @@ TEST(FrameTests, SilentFrame) {
     EXPECT_EQ(frame.toBinary(), "0000");
 }
 
-/*
-TEST(FrameTests, RepeatFrame) {
-
-}
- */
-
-// An unvoiced frame has an energy parameter, zero pitch, a repeat bit, and four reflector coefficients
-TEST(FrameTests, UnvoicedFrame) {
+TEST(FrameTests, UnvoicedFrameHasEnergyNoPitchRepeatBitAndCoeffs) {
     auto frame = frameTestSubject();
     frame.setVoicing(false);
-
     auto bin = frame.toBinary();
 
-    // energy[4] + repeat[1] + pitch[6] + K1[5] + K2[5] + K3[4] + K3[4] = 29 bits
+    // energy[4] + repeat[1] + pitch[6] +
+    // K1[5] + K2[5] + K3[4] + K3[4] = 29 bits
     EXPECT_EQ(bin.size(), 29);
 
     // Energy = 38 ~= 52 @ [1]
-    auto energyBin = bin.substr(0, 4);
-    EXPECT_EQ(energyBin, "0001");
+    auto energy_bin = bin.substr(0, 4);
+    EXPECT_EQ(energy_bin, "0001");
 
     // Repeat = 0
-    auto repeatBin = bin.substr(4, 1);
-    EXPECT_EQ(repeatBin, "0");
+    auto repeat_bin = bin.substr(4, 1);
+    EXPECT_EQ(repeat_bin, "0");
 
     // Pitch = 38 ~= 38 @ [23]
     // For unvoiced frames, pitch is ALWAYS b000000
-    auto pitchBin = bin.substr(5, 6);
-    EXPECT_EQ(pitchBin, "000000");
+    auto pitch_bin = bin.substr(5, 6);
+    EXPECT_EQ(pitch_bin, "000000");
 
     // K1 = -0.753234 ~= -0.74058 @ [17]
-    auto k1Bin = bin.substr(11, 5);
-    EXPECT_EQ(k1Bin, "10001");
+    auto k1_bin = bin.substr(11, 5);
+    EXPECT_EQ(k1_bin, "10001");
 
     // K2 = 0.939525 ~= 0.92988 @ [30]
-    auto k2Bin = bin.substr(16, 5);
-    EXPECT_EQ(k2Bin, "11110");
+    auto k2_bin = bin.substr(16, 5);
+    EXPECT_EQ(k2_bin, "11110");
 
     // K3 = -0.342255 ~= -0.33333 @ [5]
-    auto k3Bin = bin.substr(21, 4);
-    EXPECT_EQ(k3Bin, "0101");
+    auto k3_bin = bin.substr(21, 4);
+    EXPECT_EQ(k3_bin, "0101");
 
     // K4 = -0.172317 ~= 0.20579 @ [4]
-    auto k4Bin = bin.substr(25, 4);
-    EXPECT_EQ(k4Bin, "0100");
+    auto k4_bin = bin.substr(25, 4);
+    EXPECT_EQ(k4_bin, "0100");
 }
 
-// A voiced frame will have a full set of parameters
-TEST(FrameTests, VoicedFrame) {
+TEST(FrameTests, VoicedFrameHasFullParameterSet) {
     auto frame = frameTestSubject();
     auto bin = frame.toBinary();
 
     EXPECT_EQ(bin.size(), 50);
 
     // Energy = 38 ~= 52 @ [1]
-    auto energyBin = bin.substr(0, 4);
-    EXPECT_EQ(energyBin, "0001");
+    auto energy_bin = bin.substr(0, 4);
+    EXPECT_EQ(energy_bin, "0001");
 
     // Repeat = 0
-    auto repeatBin = bin.substr(4, 1);
-    EXPECT_EQ(repeatBin, "0");
+    auto repeat_bin = bin.substr(4, 1);
+    EXPECT_EQ(repeat_bin, "0");
 
     // Pitch = 38 ~= 38 @ [24]
-    auto pitchBin = bin.substr(5, 6);
-    EXPECT_EQ(pitchBin, "011000");
+    auto pitch_bin = bin.substr(5, 6);
+    EXPECT_EQ(pitch_bin, "011000");
 
     // K1 = -0.753234 ~= -0.74058 @ [17]
-    auto k1Bin = bin.substr(11, 5);
-    EXPECT_EQ(k1Bin, "10001");
+    auto k1_bin = bin.substr(11, 5);
+    EXPECT_EQ(k1_bin, "10001");
 
     // K2 = 0.939525 ~= 0.92988 @ [30]
-    auto k2Bin = bin.substr(16, 5);
-    EXPECT_EQ(k2Bin, "11110");
+    auto k2_bin = bin.substr(16, 5);
+    EXPECT_EQ(k2_bin, "11110");
 
     // K3 = -0.342255 ~= -0.33333 @ [5]
-    auto k3Bin = bin.substr(21, 4);
-    EXPECT_EQ(k3Bin, "0101");
+    auto k3_bin = bin.substr(21, 4);
+    EXPECT_EQ(k3_bin, "0101");
 
     // K4 = -0.172317 ~= 0.20579 @ [4]
-    auto k4Bin = bin.substr(25, 4);
-    EXPECT_EQ(k4Bin, "0100");
+    auto k4_bin = bin.substr(25, 4);
+    EXPECT_EQ(k4_bin, "0100");
 
     // K5 = 0.108887 ~= 0.08533 @ [8]
-    auto k5Bin = bin.substr(29, 4);
-    EXPECT_EQ(k5Bin, "1000");
+    auto k5_bin = bin.substr(29, 4);
+    EXPECT_EQ(k5_bin, "1000");
 
     // K6 = 0.679660 ~= 0.71333 @ [14]
-    auto k6Bin = bin.substr(33, 4);
-    EXPECT_EQ(k6Bin, "1110");
+    auto k6_bin = bin.substr(33, 4);
+    EXPECT_EQ(k6_bin, "1110");
 
     // K7 = 0.056874 ~= 0.05333 @ [7]
-    auto k7Bin = bin.substr(37, 4);
-    EXPECT_EQ(k7Bin, "0111");
+    auto k7_bin = bin.substr(37, 4);
+    EXPECT_EQ(k7_bin, "0111");
 
     // K8 = 0.433271 ~= 0.42857 @ [5]
-    auto k8Bin = bin.substr(41, 3);
-    EXPECT_EQ(k8Bin, "101");
+    auto k8_bin = bin.substr(41, 3);
+    EXPECT_EQ(k8_bin, "101");
 
     // K9 = -0.220355 ~= -0.18571 @ [2]
-    auto k9Bin = bin.substr(44, 3);
-    EXPECT_EQ(k9Bin, "010");
+    auto k9_bin = bin.substr(44, 3);
+    EXPECT_EQ(k9_bin, "010");
 
     // K10 = 0.17028 ~= 0.17143 @ [4]
-    auto k10Bin = bin.substr(47, 3);
-    EXPECT_EQ(k10Bin, "100");
+    auto k10_bin = bin.substr(47, 3);
+    EXPECT_EQ(k10_bin, "100");
 }
 
 };  // namespace tms_express
